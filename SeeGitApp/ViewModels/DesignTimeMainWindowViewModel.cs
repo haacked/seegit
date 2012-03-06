6c54d6b2cacc520ea68931ae35e78b99617f6dc7
Haacked <haacked@gmail.com>
2012-03-06 11:17:56 -0800

    Moved the branch adornments to a visibleposition

--- SeeGitApp/ViewModels/DesignTimeMainWindowViewModel.cs
+++ SeeGitApp/ViewModels/DesignTimeMainWindowViewModel.cs
@@ -7,6 +7,7 @@ namespace SeeGit
     {
         public DesignTimeMainWindowViewModel() : base(Dispatcher.CurrentDispatcher, _ => new DesignTimeGraphBuilder())
         {
+            Graph = new DesignTimeGraphBuilder().Graph();
         }
     }
 
@@ -20,7 +21,9 @@ namespace SeeGit
                               new CommitVertex("c34173273", "Wrote some code")
                               {Description = "This is a long form description of the commit"},
                               new CommitVertex("b1ae7a123", "Initial commit"),
-                              new CommitVertex("aa3823ca1", "Added readme")
+                              new CommitVertex("aa3823ca1", "Added readme"),
+                              new CommitVertex("9e21435fa", "Branching")
+                              {Description = "This is a long form description of the commit"}
                           };
 
             commits.First().Branches.Add("master");
@@ -29,6 +32,8 @@ namespace SeeGit
             graph.AddVertexRange(commits);
             graph.AddEdge(new CommitEdge(commits[1], commits[2]));
             graph.AddEdge(new CommitEdge(commits[0], commits[1]));
+            graph.AddEdge(new CommitEdge(commits[3], commits[2]));
+            graph.LayoutAlgorithmType = "EfficientSugiyama";
             return graph;
         }
     }
