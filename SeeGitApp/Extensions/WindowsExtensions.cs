e9b0d752235ebc67b8cee29bbddcb3c91a1096f4
chris way <chris@darksquare.net>
2012-03-19 21:49:03 +0000

    Made sure the last directory the user selected is remembered and set to the starting one if they browse for another repository.

--- SeeGitApp/Extensions/WindowsExtensions.cs
+++ SeeGitApp/Extensions/WindowsExtensions.cs
@@ -1,18 +1,24 @@
-﻿using Microsoft.WindowsAPICodePack.Dialogs;
+﻿using System.IO;
+using Microsoft.WindowsAPICodePack.Dialogs;
 
 namespace SeeGit
 {
     public static class WindowsExtensions
     {
+        private static string _lastDirectory;
+
         public static string BrowseForFolder(string startingPath)
         {
             var cfd = new CommonOpenFileDialog
                       {
-                          InitialDirectory = startingPath,
+                          InitialDirectory = _lastDirectory ?? startingPath,
                           IsFolderPicker = true,
                       };
 
-            if (cfd.ShowDialog() != CommonFileDialogResult.Ok) return null;
+            if (cfd.ShowDialog() == CommonFileDialogResult.Ok)
+                _lastDirectory = Path.GetDirectoryName(cfd.FileName);
+            else
+                return null;
 
             var ret = cfd.FileName;
             return ret;
